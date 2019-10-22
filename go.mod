e759252ed2e26263dd73ecd0edf9faf6b1e7942e
tianhai82 <tianhai82@yahoo.com.sg>
2019-10-22 21:26:47 +0800

    use gin cors to allow firebase domain access

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tianhai82/stock-timing
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/martian v2.1.0+incompatible
 	github.com/pkg/errors v0.8.1
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Stock Timing starting")
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://stock-timing.web.app"},
+	}))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
