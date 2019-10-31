54d2ff2a3436cf5df19bae8816d5fc938eda6ab4
Tan Yin Loo <tanyinloo@mrc-asia.com>
2019-10-31 14:42:39 +0800

    fix candle color

--- web/src/components/CandleChart.svelte
+++ web/src/components/CandleChart.svelte
@@ -11,11 +11,8 @@
     data = candles.map(c => [c.Open, c.Close, c.Low, c.High]);
     option = {
       ...optionTemplate,
-      xAxis: [
-        { ...optionTemplate.xAxis[0], data: dates },
-        { ...optionTemplate.xAxis[1], data: dates }
-      ],
-      series: [{ ...optionTemplate.series[0], data }]
+      xAxis: { ...optionTemplate.xAxis, data: dates },
+      series: { ...optionTemplate.series, data }
     };
   }
 
@@ -33,7 +30,7 @@
     "#c4ccd3"
   ];
 
-  let optionTemplate = {
+  const optionTemplate = {
     animation: false,
     color: colorList,
     tooltip: {
@@ -52,18 +49,17 @@
         var obj = {
           top: 60
         };
-        obj[["left", "right"][+(pos[0] < size.viewSize[0] / 2)]] = 5;
+        obj[["left", "right"][+(pos[0] < size.viewSize[0] / 2)]] = 10;
         return obj;
       }
     },
     dataZoom: [
       {
         type: "slider",
-        // xAxisIndex: [0, 1],
         realtime: false,
-        start: 50,
+        start: 75,
         end: 100,
-        top: 15,
+        top: 5,
         height: 20,
         handleIcon:
           "M10.7,11.9H9.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4h1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
@@ -71,128 +67,78 @@
       },
       {
         type: "inside",
-        // xAxisIndex: [0, 1],
         start: 50,
         end: 70,
         top: 30,
         height: 20
       }
     ],
-    xAxis: [
-      {
-        type: "category",
-        data: dates,
-        boundaryGap: false,
-        axisLine: { lineStyle: { color: "#777" } },
-        axisLabel: {
-          formatter: function(value) {
-            return echarts.format.formatTime("MM-dd", value);
-          }
+    xAxis: {
+      type: "category",
+      data: [],
+      boundaryGap: false,
+      axisLine: { lineStyle: { color: "#777" } },
+      axisLabel: {
+        formatter: function(value) {
+          return echarts.format.formatTime("MM-dd", value);
+        }
+      },
+      min: "dataMin",
+      max: "dataMax",
+      axisPointer: {
+        type: "shadow",
+        label: { show: false },
+        triggerTooltip: true,
+        handle: {
+          show: true,
+          margin: 30,
+          color: "#B80C00"
+        }
+      }
+    },
+    yAxis: {
+      scale: true,
+      splitNumber: 2,
+      axisLine: { lineStyle: { color: "#777" } },
+      splitLine: { show: true },
+      axisTick: { show: false },
+      axisLabel: {
+        inside: true,
+        formatter: "{value}\n"
+      }
+    },
+    grid: {
+      left: 2,
+      right: 8,
+      top: 5,
+      height: 260
+    },
+    graphic: {
+      type: "group",
+      left: "center",
+      top: 70,
+      width: 300,
+      bounding: "raw"
+    },
+    series: {
+      type: "candlestick",
+      name: "Price",
+      data: [],
+      itemStyle: {
+        normal: {
+          color: "#14b143",
+          color0: "#ef232a",
+          borderColor: "#14b143",
+          borderColor0: "#ef232a"
         },
-        min: "dataMin",
-        max: "dataMax",
-        axisPointer: {
-          type: "shadow",
-          label: { show: false },
-          triggerTooltip: true,
-          handle: {
-            show: true,
-            margin: 30,
-            color: "#B80C00"
-          }
+        emphasis: {
+          color: "black",
+          color0: "#444",
+          borderColor: "black",
+          borderColor0: "#444"
         }
       }
-      // {
-      //   type: "category",
-      //   gridIndex: 1,
-      //   data: dates,
-      //   scale: true,
-      //   boundaryGap: false,
-      //   splitLine: { show: false },
-      //   axisLabel: { show: false },
-      //   axisTick: { show: false },
-      //   axisLine: { lineStyle: { color: "#777" } },
-      //   splitNumber: 20,
-      //   min: "dataMin",
-      //   max: "dataMax",
-      //   axisPointer: {
-      //     type: "shadow",
-      //     label: { show: false },
-      //     triggerTooltip: true,
-      //     handle: {
-      //       show: true,
-      //       margin: 30,
-      //       color: "#B80C00"
-      //     }
-      //   }
-      // }
-    ],
-    yAxis: [
-      {
-        scale: true,
-        splitNumber: 2,
-        axisLine: { lineStyle: { color: "#777" } },
-        splitLine: { show: true },
-        axisTick: { show: false },
-        axisLabel: {
-          inside: true,
-          formatter: "{value}\n"
-        }
-      }
-      // {
-      //   scale: true,
-      //   gridIndex: 1,
-      //   splitNumber: 2,
-      //   axisLabel: { show: false },
-      //   axisLine: { show: false },
-      //   axisTick: { show: false },
-      //   splitLine: { show: false }
-      // }
-    ],
-    grid: [
-      {
-        left: 0,
-        right: 20,
-        top: 10,
-        height: 260
-      }
-      // {
-      //   left: 20,
-      //   right: 20,
-      //   height: 40,
-      //   top: 260
-      // }
-    ],
-    graphic: [
-      {
-        type: "group",
-        left: "center",
-        top: 70,
-        width: 300,
-        bounding: "raw"
-      }
-    ],
-    series: [
-      {
-        type: "candlestick",
-        name: "Price",
-        data: data,
-        itemStyle: {
-          normal: {
-            color: "#ef232a",
-            color0: "#14b143",
-            borderColor: "#ef232a",
-            borderColor0: "#14b143"
-          },
-          emphasis: {
-            color: "black",
-            color0: "#444",
-            borderColor: "black",
-            borderColor0: "#444"
-          }
-        }
-      }
-    ]
+    }
   };
 </script>
 
